72c0f23a3b66a577bcc39954c2877c6d1a664bae
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-07 22:26:44 +0900

    UIViewController Extension => id값을 반환하는 타입 연산 프로퍼티 생성

---
+++ CodebasePractice/Extension/UIViewController+.swift
@@ -0,0 +1,14 @@
+//
+//  UIViewController+.swift
+//  CodebasePractice
+//
+//  Created by Minjae Kim on 6/7/24.
+//
+
+import UIKit
+
+extension UIViewController {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
