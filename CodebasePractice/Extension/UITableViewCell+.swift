caab97b7d0062b346fa3bfd1258b71c080b5ee94
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-07 22:35:32 +0900

    UITableViewCell Extension => id값을 반환하는 타입 연산 프로퍼티 생성

---
+++ CodebasePractice/Extension/UITableViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+.swift
+//  CodebasePractice
+//
+//  Created by Minjae Kim on 6/7/24.
+//
+
+import UIKit
+
+extension UITableViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
