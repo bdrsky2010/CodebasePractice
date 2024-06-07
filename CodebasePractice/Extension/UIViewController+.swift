c3bf911625e4f3761f5dc22d7bce0e33530300fa
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-07 22:42:32 +0900

    Alert를 띄워주는 메서드가 공통적으로 사용되어 중복 코드를 방지하기 위해 UIVIewController의 extension으로 해당 메서드 코드 이동

--- CodebasePractice/Extension/UIViewController+.swift
+++ CodebasePractice/Extension/UIViewController+.swift
@@ -11,4 +11,19 @@ extension UIViewController {
     static var identifier: String {
         return String(describing: self)
     }
+    
+    public func presentErrorAlert() {
+        // 1. alert 창 구성
+        let title = "오류가 발생했어요... 🤔"
+        let alert = UIAlertController(title: title,
+                                      message: nil,
+                                      preferredStyle: .alert)
+        // 2. alert button 구성
+        let dismiss = UIAlertAction(title: "확인", style: .default)
+        
+        // 3. alert에 button 추가
+        alert.addAction(dismiss)
+        
+        present(alert, animated: true)
+    }
 }
