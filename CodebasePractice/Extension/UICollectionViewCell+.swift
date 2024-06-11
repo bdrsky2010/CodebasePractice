58bba2f1a27b3a84d37abe3f6e3439a66f71346e
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-11 23:33:31 +0900

    id값 연산프로퍼티

---
+++ CodebasePractice/Extension/UICollectionViewCell+.swift
@@ -0,0 +1,14 @@
+//
+//  UICollectionViewCell+.swift
+//  CodebasePractice
+//
+//  Created by Minjae Kim on 6/11/24.
+//
+
+import UIKit
+
+extension UICollectionViewCell {
+    static var identifier: String {
+        return String(describing: self)
+    }
+}
