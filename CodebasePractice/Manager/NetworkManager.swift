aa80a4e192473bd29d766b92e5752e85c368c23a
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-25 16:56:09 +0900

    💾 Add: 매개변수 타입 수정된 completionHandler를 사용한 NetworkManager 생성

---
+++ CodebasePractice/Manager/NetworkManager.swift
@@ -0,0 +1,34 @@
+//
+//  NetworkManager.swift
+//  CodebasePractice
+//
+//  Created by Minjae Kim on 6/25/24.
+//
+
+import Foundation
+
+import Alamofire
+
+final class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() { }
+    
+    func requestAPI<T: Decodable>(urlString: String,
+                                  method: HTTPMethod,
+                                  parameters: Parameters? = nil,
+                                  encoding: URLEncoding,
+                                  headers: HTTPHeaders? = nil,
+                                  of type: T.Type,
+                                  completionHandler: @escaping(Result<T, Error>) -> Void) {
+        AF.request(urlString, method: .get, parameters: parameters, encoding: encoding, headers: headers)
+            .responseDecodable(of: T.self) { response in
+            switch response.result {
+            case .success(let value):
+                completionHandler(.success(value))
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+}
