5f4e0cb4506ddde67206cba24d8be5c13527b192
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-25 17:46:33 +0900

    🗑️ Remove: collectionView 구성을 tableViewCell이 아닌 ViewController에서 해줌

--- CodebasePractice/Media/MovieTrend/Recommend/RecommendMovieTableViewCell.swift
+++ CodebasePractice/Media/MovieTrend/Recommend/RecommendMovieTableViewCell.swift
@@ -11,20 +11,15 @@ import SnapKit
 
 final class RecommendMovieTableViewCell: UITableViewCell {
     
-    private let collectionView = RecommendMovieCollectionView()
+    let collectionView = UICollectionView(frame: .zero, collectionViewLayout: flowLayout())
     
-    private var movieList: [TMDBMovie] = []
-    private var posterPathList: [String] = []
-    
-    var data: Decodable? {
-        didSet {
-            if let data = data as? [TMDBMovie] {
-                movieList = data
-            } else if let data = data as? [String] {
-                posterPathList = data
-            }
-            collectionView.reloadData()
-        }
+    static func flowLayout() -> UICollectionViewFlowLayout {
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.scrollDirection = .horizontal
+        flowLayout.minimumInteritemSpacing = 0
+        flowLayout.minimumLineSpacing = 8
+        flowLayout.sectionInset = .init(top: 0, left: 8, bottom: 0, right: 8)
+        return flowLayout
     }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -34,55 +29,10 @@ final class RecommendMovieTableViewCell: UITableViewCell {
         collectionView.snp.makeConstraints { make in
             make.edges.equalToSuperview()
         }
-        
-        collectionView.delegate = self
-        collectionView.dataSource = self
-        collectionView.register(RecommendMovieCollectionViewCell.self,
-                                forCellWithReuseIdentifier: RecommendMovieCollectionViewCell.identifier)
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-}
-
-extension RecommendMovieTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return !movieList.isEmpty ? movieList.count : posterPathList.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: RecommendMovieCollectionViewCell.identifier, for: indexPath) as? RecommendMovieCollectionViewCell else { return UICollectionViewCell() }
-        
-        let index = indexPath.row
-        
-        var imagePath: String
-        
-        if !movieList.isEmpty {
-            let movie = movieList[index]
-            imagePath = movie.poster_path
-        } else {
-            imagePath = posterPathList[index]
-        }
-        cell.configureContent(imagePath: imagePath)
-        
-        return cell
-    }
-}
-
-extension RecommendMovieTableViewCell: UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        
-        let totalWidth = collectionView.bounds.width
-        
-        if !movieList.isEmpty {
-            let width = totalWidth / 3 - 20
-            return CGSize(width: width, height: width * 1.5)
-        } else {
-            let width = totalWidth / 2 - 30
-            return CGSize(width: width, height: width * 1.5)
-        }
     }
 }
