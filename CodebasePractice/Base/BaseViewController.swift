1f11ea7fcc7daeb08c87926fa937b18f66aaceaf
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-26 22:47:40 +0900

    🗑️ Remove: 공통적으로 사용되지 않는 메서드 삭제

--- CodebasePractice/Base/BaseViewController.swift
+++ CodebasePractice/Base/BaseViewController.swift
@@ -11,13 +11,11 @@ class BaseViewController: UIViewController, ConfigureViewProtocol {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        configureNavigation()
         configureView()
         configureHierarchy()
         configureLayout()
     }
     
-    func configureNavigation() { }
     func configureView() { }
     func configureHierarchy() { }
     func configureLayout() { }
