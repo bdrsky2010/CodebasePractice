d160ffd2cde9e593b27e998e9b07f2d3b022aaf2
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-26 16:52:02 +0900

    💾 Add: Base로 쓰여질 뷰 추가

---
+++ CodebasePractice/Base/BaseView.swift
@@ -0,0 +1,27 @@
+//
+//  BaseView.swift
+//  CodebasePractice
+//
+//  Created by Minjae Kim on 6/25/24.
+//
+
+import UIKit
+
+class BaseView: UIView, ConfigureViewProtocol {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureView()
+        configureHierarchy()
+        configureLayout()
+    }
+    
+    func configureView() { }
+    func configureHierarchy() { }
+    func configureLayout() { }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
