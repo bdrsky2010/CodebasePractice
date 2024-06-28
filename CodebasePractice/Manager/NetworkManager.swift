a74c15b1005c1af12b7e3e4f80e925eefe1a0ac9
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-06-28 14:56:16 +0900

    💾 Add: URLSession으로 변환작업중 테스트 필요
     - 에러 상태에 따라 alert를 보여주기 위해 enum에 case 추가 및 연산 프로퍼티 추가(타이틀, 메세지)

--- CodebasePractice/Manager/NetworkManager.swift
+++ CodebasePractice/Manager/NetworkManager.swift
@@ -10,10 +10,46 @@ import Foundation
 import Alamofire
 
 enum NetworkError: Error {
-    case failedRequest
+    case unknown
     case noData
-    case invalidResponse
+    case failedRequest
     case invalidData
+    case invalidClientResponse
+    case invalidServerResponse
+    
+    var alertTitle: String {
+        switch self {
+        case .unknown:
+            return "알 수 없는 에러"
+        case .noData:
+            return "존재하지 않는 데이터"
+        case .failedRequest:
+            return "요청 실패"
+        case .invalidData:
+            return "알 수 없는 데이터"
+        case .invalidClientResponse:
+            return "클라이언트 요청 에러"
+        case .invalidServerResponse:
+            return "서버 에러"
+        }
+    }
+    
+    var alertMessage: String {
+        switch self {
+        case .unknown:
+            return "알 수 없는 에러로 데이터를 받아오지 못하였습니다"
+        case .noData:
+            return "데이터가 존재하지 않아 에러가 발생하였습니다"
+        case .failedRequest:
+            return "요청에 실패하여 데이터를 받아오지 못하였습니다"
+        case .invalidData:
+            return "알 수 없는 데이터로 인해 에러가 발생하였습니다"
+        case .invalidClientResponse:
+            return "클라이언트에서의 네트워크 연결 혹은 입력값이 잘못되어 데이터를 받아오지 못하였습니다"
+        case .invalidServerResponse:
+            return "서버의 문제가 생겨 데이터를 받아오지 못하였습니다"
+        }
+    }
 }
 
 final class NetworkManager {
@@ -56,13 +92,24 @@ final class NetworkManager {
                 
                 guard let response = response as? HTTPURLResponse else {
                     print("Unable Response")
-                    completionHandler(.failure(.invalidResponse))
+                    completionHandler(.failure(.invalidClientResponse))
                     return
                 }
                 
-                guard response.statusCode == 200 else {
-                    print("failed Response")
-                    completionHandler(.failure(.invalidResponse))
+                switch response.statusCode {
+                case 200..<300:
+                    break
+                case 400..<500:
+                    print("failed response")
+                    completionHandler(.failure(.invalidClientResponse))
+                    return
+                case 500..<600:
+                    print("failed response")
+                    completionHandler(.failure(.invalidServerResponse))
+                    return
+                default:
+                    print("failed response")
+                    completionHandler(.failure(.unknown))
                     return
                 }
                 
