83c154b8e47104914ef0e7159e934c65d0b6c59b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-07-03 18:10:09 +0900

    🗑️ Remove & 💾 Add: 콜렉션뷰를 테이블뷰 셀로 이동하면서 테이블뷰 추가 및 콜렉션뷰 삭제

--- CodebasePractice/Reminder/View/ReminderMainView.swift
+++ CodebasePractice/Reminder/View/ReminderMainView.swift
@@ -11,35 +11,18 @@ import SnapKit
 
 final class ReminderMainView: BaseView {
     
-    lazy var reminderMainCollectionView = UICollectionView(frame: .zero, collectionViewLayout: collectionViewLayout())
-    
-    func collectionViewLayout() -> UICollectionViewLayout {
-        let layout = UICollectionViewFlowLayout()
-        if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene {
-            let width = windowScene.screen.bounds.width - 48
-            layout.itemSize = CGSize(width: width / 2, height: width / 2 * 0.45)
-            layout.scrollDirection = .vertical
-            layout.minimumLineSpacing = 10
-            layout.minimumInteritemSpacing = 10
-            layout.sectionInset = UIEdgeInsets(top: 10, left: 16, bottom: 10, right: 16)
-        }
-        return layout
-    }
+    let reminderMainTableView = UITableView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
     }
     
-    override func configureView() {
-        backgroundColor = .systemBackground
-    }
-    
     override func configureHierarchy() {
-        addSubview(reminderMainCollectionView)
+        addSubview(reminderMainTableView)
     }
     
     override func configureLayout() {
-        reminderMainCollectionView.snp.makeConstraints { make in
+        reminderMainTableView.snp.makeConstraints { make in
             make.edges.equalTo(safeAreaLayoutGuide)
         }
     }
