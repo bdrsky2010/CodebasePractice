290f43c41d469a4edb68b915b98101b196fad77e
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-07-03 18:04:44 +0900

    💾 Add: Reminder 에서 사용할 용도의 date 연산프로퍼티 extension 추가

--- CodebasePractice/Extension/Date+.swift
+++ CodebasePractice/Extension/Date+.swift
@@ -14,4 +14,11 @@ extension Date {
         dateFormmater.dateFormat = "MM월 dd일 HH시 mm분"
         return dateFormmater.string(from: Date())
     }
+    
+    var reminderString: String {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 M월 d일 EEEE"
+        return formatter.string(from: self)
+    }
 }
