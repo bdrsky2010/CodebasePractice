bbc828fda2fa27f5b7641fd871a74a8daf699b0b
SweetPumpkinCK <80156515+bdrsky2010@users.noreply.github.com>
2024-07-03 09:20:15 +0900

    💾 Add: 값 전달용 델리게이트 추가

---
+++ CodebasePractice/Reminder/Delegate/ReminderUpdateDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  ReminderUpdateDelegate.swift
+//  CodebasePractice
+//
+//  Created by Minjae Kim on 7/3/24.
+//
+
+import Foundation
+
+protocol ReminderUpdateDelegate: AnyObject {
+    func reloadMainCollectionView()
+}
